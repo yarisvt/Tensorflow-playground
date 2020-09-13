962074824246ff7632fb68488a2109eea1efdb56
Yaris van Thiel <yarisvanthiel@gmail.com>
2020-09-13 15:19:29 +0200

    added gui to predict images

--- deeplearning.py
+++ deeplearning.py
@@ -1,7 +1,15 @@
+# pylint: disable=E1101
+
+import random
+
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import List, Tuple, Any, ClassVar, Optional
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter.messagebox as messagebox
+import tkinter as tk
 
 
 class Network:
@@ -11,6 +19,7 @@ class Network:
         self.loaded_model = None
         self.activation = {'relu': tf.nn.relu, 'softmax': tf.nn.softmax}
         self.model = model
+        self.load_data()
 
     @classmethod
     def from_model(cls, file_name: str):
@@ -22,7 +31,6 @@ class Network:
                 input_shape = layer.input_shape[1:]
             elif 'Dense' in str(layer):
                 layers.append([layer.units, layer.activation.__name__])
-
         return cls(input_shape=input_shape, layers=layers, model=tf.keras.models.load_model(file_name))
 
     def load_data(self):
@@ -60,12 +68,15 @@ class Network:
     def save_model(self, file_name: str):
         self.model.save(file_name)
 
-    def predict(self, image_nr: int, draw_image: bool):
+    def predict(self, image_nr: int) -> int:
         predictions = self.model.predict([self.test_data])
-        print(
-            f'label -> {self.test_labels[image_nr]}\nprediction -> {np.argmax(predictions[image_nr])}')
-        if draw_image:
-            draw(self.test_data[image_nr])
+        return self.test_labels[image_nr], np.argmax(predictions[image_nr])
+
+    def get_test_data(self):
+        return self.test_data, self.test_labels
+
+    def get_training_data(self):
+        return self.train_data, self.train_labels
 
 
 def draw(image: np.ndarray):
@@ -82,9 +93,64 @@ def load_data():
     return x_train, y_train, x_test, y_test
 
 
-net = Network.from_model('test.h5')
-net.load_data()
-net.predict(18, draw_image=False)
+class GUI:
+    def __init__(self, model='test.h5'):
+        self.window = tk.Tk()
+        self.window.title = 'Predicting images+'
+        self.net = Network.from_model(model)
+        self.test_data, _ = self.net.get_test_data()
+        self.figure = None
+        self.top_frame = None
+        self.bottom_frame = None
+        self.figures = []
+        self.create_window(self.get_random_nr())
+
+    def create_window(self, img_range):
+        self.figure = plt.figure()
+        self.top_frame = tk.Frame(self.window)
+        self.bottom_frame = tk.Frame(self.window)
+        i = 0
+        for index in range(img_range, img_range + 9):
+            ax = self.figure.add_subplot(330 + 1 + i)
+            ax.imshow(self.test_data[index], cmap=plt.cm.binary)
+            self.figures.append([ax, index])
+            i += 1
+
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.top_frame)
+        self.canvas.mpl_connect('button_press_event', self.handle_click)
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        tk.Button(self.bottom_frame, text='Get new random images',
+                  command=self.new_images).pack()
+
+        self.top_frame.pack()
+        self.bottom_frame.pack()
+        tk.mainloop()
+
+    def handle_click(self, event):
+        for fig in self.figures:
+            if fig[0] == event.inaxes:
+                label, prediction = self.net.predict(fig[1])
+                messagebox.showinfo(
+                    'Prediction', f'I predicted: {prediction}\nTrue number: {label}')
+
+    def new_images(self):
+        self.top_frame.destroy()
+        self.bottom_frame.destroy()
+        self.figures = []
+        self.create_window(self.get_random_nr())
+
+    def get_random_nr(self):
+        return random.randrange(len(self.test_data) - 9)
+
+
+# gui = GUI()
+# gui.gui(gui.get_random_nr())
+
+
+# net = Network.from_model('test.h5')
+# net.load_data()
+# net.predict(18, draw_image=True)
 # example
 
 #  network = Network((28, 28), [
